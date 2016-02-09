174946c15c628db230b4206bedac4ba9aa49d15c
Sheraz Mahmood <sheraz.mahmood@placesbyme.com>
2016-02-09 14:49:34 -0500

    Initial Commit

---
+++ kingpin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  kingpin
+//
+//  Created by Sheraz Mahmood on 2/9/16.
+//  Copyright © 2016 smahmood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
