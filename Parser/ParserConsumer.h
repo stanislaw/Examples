5a3c8d81c3ae4bbc896915ba12dafcc262debb3e
Stanislaw Pankevich <s.pankevich@gmail.com>
2016-03-28 19:36:07 +0200

    Initial commit

---
+++ Parser/ParserConsumer.h
@@ -0,0 +1,16 @@
+//
+//  ParserConsumer.h
+//  Reentrant-Parser-Using-Flex-and-Bison
+//
+//  Created by Stanislaw Pankevich on 19/11/15.
+//  Copyright © 2015 Stanislaw Pankevich. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol ParserConsumer <NSObject>
+
+- (void)parserDidParseString:(char *)string;
+- (void)parserDidParseNumber:(int)number;
+
+@end
